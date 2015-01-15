676fe75d6fc01f8de2c82c98052425ce10596631
mainlaptop <mainlaptop@mainlaptop.com>
2015-01-15 18:59:27 -0500

    Billy/Art/Josh getting tilter/holder working

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -11,21 +11,83 @@
  * don't. Unless you know what you are doing, complex code will be much more difficult under
  * this system. Use IterativeRobot or Command-Based instead if you're new.
  */
+class Piston
+{
+public:
+	Piston(Solenoid*_fwdsol, Solenoid*_revsol):
+		offtime(Timer::GetFPGATimestamp()),
+		fwdsol(_fwdsol),
+		revsol(_revsol)
+	{
+
+	}
+	void Forward()
+	{
+		offtime=Timer::GetFPGATimestamp()+.5;
+		fwdsol->Set(true);
+		revsol->Set(false);
+	}
+	void Reverse()
+	{
+		offtime=Timer::GetFPGATimestamp()+.5;
+		fwdsol->Set(false);
+		revsol->Set(true);
+	}
+	void Neutral()
+	{
+		offtime=Timer::GetFPGATimestamp();
+				fwdsol->Set(false);
+				revsol->Set(false);
+	}
+	void Tick()
+	{
+		if (Timer::GetFPGATimestamp()>offtime)
+		{
+			Neutral();
+		}
+	}
+private:
+	double offtime;
+Solenoid*fwdsol;
+Solenoid*revsol;
+
+};
 class Robot: public OurSampleRobot
 {
+#ifdef FRC2014
+	Compressor compressor;
+#endif
 	Talon winch;
 	RobotDrive myRobot; //Our robot drive system
 	Joystick stick; // only joystick
 	Joystick opstick;
+	Solenoid tiltback;
+	Solenoid tiltforward;
+	Piston tiltpiston;
+	Solenoid closeholder;
+	Solenoid openholder;
+	Piston holderpiston;
 
 public:
 	Robot() :
+#ifdef FRC2014
+		compressor(PRESSURE_INPUT_DIO, COMPRESSOR_RELAY),
+#endif
 			winch(WINCH_PWM),
 			myRobot(DRIVE_LEFT_FRONT_PWM, DRIVE_LEFT_BACK_PWM,DRIVE_RIGHT_FRONT_PWM, DRIVE_RIGHT_BACK_PWM),	// initialize the RobotDrive to use motor controllers on ports 0 and 1
 			stick(DRIVER_JOYSTICK_PORT),
-			opstick(OPERATOR_JOYSTICK_PORT)
+			opstick(OPERATOR_JOYSTICK_PORT),
+			tiltback(TILT_BACK_SOLENOID),
+			tiltforward(TILT_FORWARD_SOLENOID),
+			tiltpiston(&tiltforward, &tiltback),
+			closeholder(CLOSE_HOLDER_SOLENOID),
+			openholder(OPEN_HOLDER_SOLENOID),
+			holderpiston(&openholder, &closeholder)
 	{
 		myRobot.SetExpiration(0.1);
+#ifdef FRC2014
+		compressor.Start();
+#endif
 	}
 
 	/**
@@ -33,11 +95,45 @@ public:
 	 */
 	void OperatorControl()
 	{
+		myRobot.SetSafetyEnabled(false);
+		winch.SetSafetyEnabled(false);
+		myRobot.SetExpiration(100);
+		winch.SetExpiration(100);
+#ifdef FRC2014
+		GetWatchdog().SetEnabled(false);
+#endif
+		
+
 		while (IsOperatorControl() && IsEnabled())
 		{
-			printf("2015 code on 2014 robot\n");
-			myRobot.ArcadeDrive(stick); // drive with arcade style (use right stick)
-			Wait(0.005);				// wait for a motor update time
+			tiltpiston.Tick();
+			holderpiston.Tick();
+
+
+#ifdef FRC2014
+			this->GetWatchdog().Feed();
+
+#endif
+			winch.Set(opstick.GetRawAxis(Joystick::kDefaultYAxis));
+			// handling the tilt piston
+			if(stick.GetRawButton(TILT_BACK_BUTTON))
+			{
+				tiltpiston.Reverse();
+			}
+			else if(stick.GetRawButton(TILT_FORWARD_BUTTON))
+			{
+				tiltpiston.Forward();
+			}
+
+			// Handling the crate holder
+			if (stick.GetRawButton(OPEN_HOLDER_BUTTON))
+			{
+				holderpiston.Reverse();
+			}
+			else if (stick.GetRawButton(CLOSE_HOLDER_BUTTON))
+			{
+				holderpiston.Forward();
+			}
 		}
 	}
 
--- src/constants.h
+++ src/constants.h
@@ -1,7 +1,29 @@
+// buttons
+const int TILT_BACK_BUTTON=3;
+const int TILT_FORWARD_BUTTON=2;
+const int OPEN_HOLDER_BUTTON=5;
+const int CLOSE_HOLDER_BUTTON=4;
+
+// PWMs
 const int WINCH_PWM=3;
 const int DRIVE_LEFT_FRONT_PWM=4;
 const int DRIVE_LEFT_BACK_PWM=5;
 const int DRIVE_RIGHT_FRONT_PWM=6;
 const int DRIVE_RIGHT_BACK_PWM=7;
-const int DRIVER_JOYSTICK_PORT=0;
-const int OPERATOR_JOYSTICK_PORT=1;
+
+// relay
+const int COMPRESSOR_RELAY = 1;
+
+// digital IO
+const int PRESSURE_INPUT_DIO = 5;
+
+// joystick ports
+const int DRIVER_JOYSTICK_PORT=1;
+const int OPERATOR_JOYSTICK_PORT=2;
+
+
+// solenoids
+const int TILT_BACK_SOLENOID=3;
+const int TILT_FORWARD_SOLENOID=4;
+const int OPEN_HOLDER_SOLENOID=5;
+const int CLOSE_HOLDER_SOLENOID=6;
